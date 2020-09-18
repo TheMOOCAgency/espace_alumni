69faea711998e187b9f150d4014a7567af41b6f0
yoann.mroz <yoann.mroz@themoocagency.com>
2020-09-18 14:07:29 +0200

    manage window.props loading

--- public/props.js
+++ public/props.js
@@ -1,62 +1,64 @@
-window.props = {
-  users_list : [
-    {
-      "city": "Brussels",
-      "first_name": "Yuri",
-      "last_name": "Guzeev",
-      "email": "champaniaque@gmail.com",
-      "country": "Belgium"
-    },
-    {
-      "city": "Makati",
-      "first_name": "Ira",
-      "last_name": "Del Rosario",
-      "email": "delrosario_ira@yahoo.com",
-      "country": "Philippines"
-    },
-    {
-      "city": "London",
-      "first_name": "Filippo",
-      "last_name": "Tacchi",
-      "email": "filippotacchi@icloud.com",
-      "country": "United Kingdom"
-    },
-    {
-      "city": "Leeds",
-      "first_name": "Paul",
-      "last_name": "Howard",
-      "email": "pchoward@btinternet.com",
-      "country": "United Kingdom"
-    },
-    {
-      "city": "Austin, Texas",
-      "first_name": "Rafael",
-      "last_name": "Tristan-Rangel",
-      "email": "r_tristan@icloud.com",
-      "country": "USA"
-    },
-    {
-      "city": "Durban",
-      "first_name": "Yegas",
-      "last_name": "Naidoo",
-      "email": "woodstr@saol.com",
-      "country": "South Africa"
-    },
-    {
-      "city": "Bruges",
-      "first_name": "Charly",
-      "last_name": "Troilo",
-      "email": "charlytroilo@gmail.com",
-      "country": "France"
+setTimeout(function() {
+  window.props = {
+    users_list : [
+      {
+        "city": "Brussels",
+        "first_name": "Yuri",
+        "last_name": "Guzeev",
+        "email": "champaniaque@gmail.com",
+        "country": "Belgium"
+      },
+      {
+        "city": "Makati",
+        "first_name": "Ira",
+        "last_name": "Del Rosario",
+        "email": "delrosario_ira@yahoo.com",
+        "country": "Philippines"
+      },
+      {
+        "city": "London",
+        "first_name": "Filippo",
+        "last_name": "Tacchi",
+        "email": "filippotacchi@icloud.com",
+        "country": "United Kingdom"
+      },
+      {
+        "city": "Leeds",
+        "first_name": "Paul",
+        "last_name": "Howard",
+        "email": "pchoward@btinternet.com",
+        "country": "United Kingdom"
+      },
+      {
+        "city": "Austin, Texas",
+        "first_name": "Rafael",
+        "last_name": "Tristan-Rangel",
+        "email": "r_tristan@icloud.com",
+        "country": "USA"
+      },
+      {
+        "city": "Durban",
+        "first_name": "Yegas",
+        "last_name": "Naidoo",
+        "email": "woodstr@saol.com",
+        "country": "South Africa"
+      },
+      {
+        "city": "Bruges",
+        "first_name": "Charly",
+        "last_name": "Troilo",
+        "email": "charlytroilo@gmail.com",
+        "country": "France"
+      }
+    ],
+    texts :{ 
+      backToCourse : "Retour au cours",
+      title : "Espace Alumni – MOOC du Champagne",
+      welcome : "Bienvenue sur l’espace Alumni du MOOC du Champagne !",
+      welcomeSubtitle : "Utilisez les filtres pour affiner votre recherche",
+      town : "ville",
+      country : "Pays",
+      contact : "contacter"
     }
-  ],
-  texts :{ 
-    backToCourse : "Retour au cours",
-    title : "Espace Alumni – MOOC du Champagne",
-    welcome : "Bienvenue sur l’espace Alumni du MOOC du Champagne !",
-    welcomeSubtitle : "Utilisez les filtres pour affiner votre recherche",
-    town : "ville",
-    country : "Pays",
-    contact : "contacter"
   }
-}
+}, 1000);
--- src/App.js
+++ src/App.js
@@ -1,32 +1,69 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react'
+import CircularProgress from '@material-ui/core/CircularProgress';
 import './App.css';
 import UserCard from './components/user_card';
 import Menu from './components/menu';
 
 
+export default class extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userList: [],
+      texts: {},
+      isLoading: true
+    }
+    this.setUserList = this.setUserList.bind(this)
+  }
+  
+  setUserList(newData) {
+    this.setState({
+      userList: newData,
+    });
+  }
 
-function App() {
-  const [userList, setUserList] = useState([...window.props.users_list]);
-  return (
-    <div className="App">
-      <Menu
-        savedList={window.props.users_list}
-        setUserList={setUserList}
-        cities={[...new Set(userList.map((user) => {return user.city}))].sort()}
-        countries={[...new Set(userList.map((user) => {return user.country}))].sort()}
-      />
-      {userList.map((user, index) => {
-        return <UserCard 
-        key={index}
-        first_name={user.first_name}
-        last_name={user.last_name}
-        email={user.email}
-        city={user.city}
-        country={user.country}
-        />
-      })}
-    </div>
-  );
+  componentWillMount() {
+    const that = this
+    const loadingProps = setInterval(function(){
+      if (window.props && window.props.users_list && window.props.texts) {
+        clearInterval(loadingProps);
+        that.setState({
+          userList: [...window.props.users_list],
+          texts: {...window.props.texts},
+          isLoading: false
+        });
+      }
+    }, 500);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isLoading ?
+        <>
+        <CircularProgress size={160} />
+        </>
+        :
+        <>
+          <Menu
+            savedList={this.state.userList}
+            setUserList={this.setUserList}
+            cities={[...new Set(this.state.userList.map((user) => {return user.city}))].sort()}
+            countries={[...new Set(this.state.userList.map((user) => {return user.country}))].sort()}
+          />
+          {this.state.userList.map((user, index) => {
+            return <UserCard 
+            key={index}
+            first_name={user.first_name}
+            last_name={user.last_name}
+            email={user.email}
+            city={user.city}
+            country={user.country}
+            />
+          })}
+        </>
+        }
+      </div>
+    );
+  }
 }
-
-export default App;
